4311637e2ad8effcdbe22da258a7228405c928a1
wutingjia <wutingjia@meituan.com>
2022-03-04 14:02:02 +0800

    modify tomcat10 plugin test

--- test/plugin/scenarios/tomcat-10.x-scenario/tomcatserver-10.x-scenario/src/main/java/org/apache/skywalking/apm/testcase/tomcatserver/servlet/HealthCheckServlet.java
+++ test/plugin/scenarios/tomcat-10.x-scenario/tomcatserver-10.x-scenario/src/main/java/org/apache/skywalking/apm/testcase/tomcatserver/servlet/HealthCheckServlet.java
@@ -25,7 +25,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-public class CaseServlet extends HttpServlet {
+public class HealthCheckServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
